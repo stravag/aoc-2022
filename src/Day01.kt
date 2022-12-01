d011a87afbf87b856186fce7be22de9e2881f83e
Ranil Wijeyratne <me@ranil.ch>
2022-12-01 09:40:11 +0100

    aoc1.1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,6 +1,18 @@
+import kotlin.math.max
+
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var maxCalories = 0
+        var caloriesCount = 0
+        input.forEach {
+            if (it.isBlank()) {
+                maxCalories = max(caloriesCount, maxCalories)
+                caloriesCount = 0
+            } else {
+                caloriesCount += it.toInt()
+            }
+        }
+        return maxCalories
     }
 
     fun part2(input: List<String>): Int {
@@ -9,7 +21,7 @@ fun main() {
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 24000)
 
     val input = readInput("Day01")
     println(part1(input))
