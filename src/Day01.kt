c51907b0efe362178baea332f0a6c5f3241c5f83
Ranil Wijeyratne <me@ranil.ch>
2022-12-01 10:29:13 +0100

    aoc1.1 functional

--- src/Day01.kt
+++ src/Day01.kt
@@ -2,28 +2,39 @@ import kotlin.math.max
 
 fun main() {
     fun part1(input: List<String>): Int {
-        var maxCalories = 0
-        var caloriesCount = 0
-        input.forEach {
-            if (it.isBlank()) {
-                maxCalories = max(caloriesCount, maxCalories)
-                caloriesCount = 0
-            } else {
-                caloriesCount += it.toInt()
+        val (max, _) = input.fold(
+            initial = 0 to 0,
+            operation = { (max, cnt), s ->
+                val calorie = s.toIntOrZero()
+                val newCnt = if (s.isEmpty()) 0 else cnt + calorie
+                val newMax = max(max, newCnt)
+                newMax to newCnt
             }
-        }
-        return maxCalories
+        )
+        return max
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val (topThree, _) = input.fold(
+            initial = emptyList<Int>() to 0,
+            operation = { (topThree, cnt), s ->
+                val calorie = s.toIntOrZero()
+                val newCnt = if (s.isEmpty()) 0 else cnt + calorie
+                val newTopThree = (topThree + newCnt).sortedDescending().take(3)
+                newTopThree to newCnt
+            }
+        )
+        return topThree.sum()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
     check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
 
     val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
+
+fun String.toIntOrZero() = this.ifEmpty { "0" }.toInt()
