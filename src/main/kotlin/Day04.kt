899b0a987192520c60ae363b377a71b117da8b00
Ranil Wijeyratne <me@ranil.ch>
2022-12-04 09:02:18 +0100

    day4.2

--- src/main/kotlin/Day04.kt
+++ src/main/kotlin/Day04.kt
@@ -8,4 +8,16 @@ object Day04 {
                 (a1 >= b1 && a2 <= b2) || (b1 >= a1 && b2 <= a2)
             }
     }
+
+    fun compute2(input: List<String>): Int {
+        return input
+            .map { it.split(",") }
+            .count { (a, b) ->
+                val (a1, a2) = a.split("-").map { it.toInt() }
+                val r1 = IntRange(a1, a2)
+                val (b1, b2) = b.split("-").map { it.toInt() }
+                val r2 = IntRange(b1, b2)
+                r1.contains(b1) || r1.contains(b2) || r2.contains(a1) || r2.contains(a2)
+            }
+    }
 }
--- src/test/kotlin/Day04Test.kt
+++ src/test/kotlin/Day04Test.kt
@@ -8,4 +8,10 @@ class Day04Test {
         assertEquals(2, Day04.compute1(readInput("Day04_test")))
         println("part 1: " + Day04.compute1(readInput("Day04")))
     }
+
+    @Test
+    fun part2() {
+        assertEquals(4, Day04.compute2(readInput("Day04_test")))
+        println("part 2: " + Day04.compute2(readInput("Day04")))
+    }
 }
