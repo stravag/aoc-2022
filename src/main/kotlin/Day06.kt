0c196eabe4c3f21d0216bfcb1b1a9d22061a2986
Ranil Wijeyratne <ranil.wijeyratne@ergon.ch>
2022-12-06 12:20:48 +0100

    day06

---
+++ src/main/kotlin/Day06.kt
@@ -0,0 +1,36 @@
+fun main() {
+    execute(
+        day = "Day06",
+        part1 = listOf(5, 6, 10, 11) to ::compute1,
+        part1Test = listOf(
+            "bvwbjplbgvbhsrlpgdmjqwftvncz",
+            "nppdvjthqldpwncqszvftbrmjlhg",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+        ),
+        part2 = listOf(19, 23, 23, 29, 26) to ::compute2,
+        part2Test = listOf(
+            "mjqjpqmgbljsphdztnvjfqwrcgsmlb",
+            "bvwbjplbgvbhsrlpgdmjqwftvncz",
+            "nppdvjthqldpwncqszvftbrmjlhg",
+            "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg",
+            "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw",
+        ),
+    )
+}
+
+private fun compute1(input: List<String>): List<Int> {
+    return input.map { it.findSignalLoc(4) }
+}
+
+private fun compute2(input: List<String>): List<Int> {
+    return input.map { it.findSignalLoc(14) }
+}
+
+private fun String.findSignalLoc(signalSize: Int): Int {
+    for (i in signalSize until length) {
+        val chunk = subSequence(i - signalSize, i)
+        if (chunk.toSet().size == signalSize) return i
+    }
+    throw IllegalStateException("no signal found")
+}
--- src/main/kotlin/Utils.kt
+++ src/main/kotlin/Utils.kt
@@ -2,16 +2,16 @@ import java.io.File
 
 fun <R1, R2> execute(
     day: String,
-    part1: Pair<R1, (List<String>) -> R1>,
-    part2: Pair<R2, (List<String>) -> R2>,
+    part1: Pair<R1, (List<String>) -> R1>, part1Test: List<String> = readInput("${day}_test.txt"),
+    part2: Pair<R2, (List<String>) -> R2>, part2Test: List<String> = readInput("${day}_test.txt"),
 ) {
     val (expected1, method1) = part1
-    val answer1 = method1(readInput("${day}_test.txt"))
+    val answer1 = method1(part1Test)
     check(expected1 == answer1) { "expected $expected1 but got $answer1" }
     println("part1: " + method1(readInput("${day}.txt")))
 
     val (expected2, method2) = part2
-    val answer2 = method2(readInput("${day}_test.txt"))
+    val answer2 = method2(part2Test)
     check(expected2 == answer2) { "expected $expected2 but got $answer2" }
     println("part2: " + method2(readInput("${day}.txt")))
 }
