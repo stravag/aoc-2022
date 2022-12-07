4ac6a89337c9e338bf3c13c5a2ca5cd55e99fba9
Ranil Wijeyratne <me@ranil.ch>
2022-12-07 23:08:25 +0100

    day 7 fix

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -9,59 +9,51 @@ fun main() {
 private fun compute1(input: List<String>): Int {
     val folders = parse(input)
     return folders
-        .map { it.size }
+        .map { it.value }
         .filter { it <= 100000 }
         .sum()
 }
 
 private fun compute2(input: List<String>): Int {
     val folders = parse(input)
-    val unusedSpace = 70000000 - folders.first().size
+    val unusedSpace = 70000000 - folders.entries.first().value
     val requiredSpace = 30000000 - unusedSpace
 
     return folders
-        .map { it.size }
+        .map { it.value }
         .filter { it >= requiredSpace }
         .min()
 }
 
-private fun parse(input: List<String>): List<FolderInfo> {
-    val folders = mutableListOf<FolderInfo>()
-    var pointer = -1
+private fun parse(input: List<String>): Map<String, Int> {
+    val folders = linkedMapOf<String, Int>()
+    val currentPath = mutableListOf<String>()
     input.forEach { line ->
         when {
-            line == "$ ls" -> Unit
-            line.startsWith("dir") -> Unit
-            line.startsWith("$ cd") -> {
+            line.isChangeDir() -> {
                 when (val dirName = line.split(" ")[2]) {
-                    ".." -> {
-                        // propagate size to parent
-                        val currentFolder = folders[pointer]
-                        val parentFolder = folders[pointer - 1]
-                        folders[pointer - 1] = parentFolder.increaseSize(currentFolder)
-                        pointer--
+                    ".." -> currentPath.removeLast()
+                    else -> {
+                        currentPath.add(dirName)
+                        folders[currentPath.joinToString()] = 0
                     }
-
-                    else -> folders.add(++pointer, FolderInfo(dirName, 0))
                 }
             }
 
-            else -> {
-                val size = line.split(" ").first().toInt()
-                val currentFolder = folders[pointer]
-                folders[pointer] = currentFolder.increaseSize(size)
+            line.isFile() -> {
+                val fileSize = line.split(" ").first().toInt()
+                currentPath
+                    .runningFold(emptyList<String>()) { acc, s -> acc + s }
+                    .forEach { path ->
+                        folders.computeIfPresent(path.joinToString()) { _, folderSize -> folderSize + fileSize }
+                    }
+
             }
         }
     }
 
-    // propagate size to root
-    val currentFolder = folders[pointer]
-    val parentFolder = folders[pointer - 1]
-    folders[pointer - 1] = parentFolder.increaseSize(currentFolder)
     return folders
 }
 
-private data class FolderInfo(val name: String, val size: Int) {
-    fun increaseSize(added: Int) = copy(size = size + added)
-    fun increaseSize(folder: FolderInfo) = copy(size = size + folder.size)
-}
+fun String.isChangeDir() = startsWith("$ cd")
+fun String.isFile() = matches("^[0-9]+ .*".toRegex())
