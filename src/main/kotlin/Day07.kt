0f8fdc12556547aafa844290fef043dc84d258e8
Ranil Wijeyratne <me@ranil.ch>
2022-12-07 22:30:38 +0100

    day 7.1 test ok

--- src/main/kotlin/Day07.kt
+++ src/main/kotlin/Day07.kt
@@ -2,12 +2,30 @@ fun main() {
     execute(
         day = "Day07",
         part1 = 95437 to ::compute1,
-        part2 = 0 to ::compute2,
+        part2 = 24933642 to ::compute2,
     )
 }
 
 private fun compute1(input: List<String>): Int {
+    val folders = parse(input)
+    return folders
+        .map { it.size }
+        .filter { it <= 100000 }
+        .sum()
+}
 
+private fun compute2(input: List<String>): Int {
+    val folders = parse(input)
+    val unusedSpace = 70000000 - folders.first().size
+    val requiredSpace = 30000000 - unusedSpace
+
+    return folders
+        .map { it.size }
+        .filter { it >= requiredSpace }
+        .min()
+}
+
+private fun parse(input: List<String>): List<FolderInfo> {
     val folders = mutableListOf<FolderInfo>()
     var pointer = -1
     input.forEach { line ->
@@ -17,14 +35,17 @@ private fun compute1(input: List<String>): Int {
             line.startsWith("$ cd") -> {
                 when (val dirName = line.split(" ")[2]) {
                     ".." -> {
+                        // propagate size to parent
                         val currentFolder = folders[pointer]
                         val parentFolder = folders[pointer - 1]
                         folders[pointer - 1] = parentFolder.increaseSize(currentFolder)
                         pointer--
                     }
+
                     else -> folders.add(++pointer, FolderInfo(dirName, 0))
                 }
             }
+
             else -> {
                 val size = line.split(" ").first().toInt()
                 val currentFolder = folders[pointer]
@@ -33,22 +54,14 @@ private fun compute1(input: List<String>): Int {
         }
     }
 
+    // propagate size to root
     val currentFolder = folders[pointer]
     val parentFolder = folders[pointer - 1]
     folders[pointer - 1] = parentFolder.increaseSize(currentFolder)
-    pointer--
-
     return folders
-        .map { it.size }
-        .filter { it <= 100000 }
-        .sum()
 }
 
-data class FolderInfo(val name: String, val size: Int) {
+private data class FolderInfo(val name: String, val size: Int) {
     fun increaseSize(added: Int) = copy(size = size + added)
     fun increaseSize(folder: FolderInfo) = copy(size = size + folder.size)
-}
-
-private fun compute2(input: List<String>): Int {
-    return input.size
 }
