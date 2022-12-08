3772a56f13ee7025c7ff81404b64d87c364af115
Ranil Wijeyratne <me@ranil.ch>
2022-12-08 07:11:24 +0100

    day 8.1 test working

--- src/main/kotlin/Day08.kt
+++ src/main/kotlin/Day08.kt
@@ -1,15 +1,45 @@
 fun main() {
     execute(
         day = "Day08",
-        part1 = 0 to ::compute1,
+        part1 = 21 to ::compute1,
         part2 = 0 to ::compute2,
     )
 }
 
 private fun compute1(input: List<String>): Int {
-    return input.size
+    val trees: Forrest = input
+        .map { row -> row.toCharArray().map { it.code } }
+
+    val visibleTrees = trees.flatMapIndexed { rowIdx, treeRow ->
+        treeRow.filterIndexed { colIdx, _ ->
+            trees.isVisible(rowIdx, colIdx)
+        }
+    }
+
+    return visibleTrees.count()
 }
 
 private fun compute2(input: List<String>): Int {
     return input.size
 }
+
+private fun Forrest.isVisible(rowIdx: Int, colIdx: Int): Boolean {
+    val tree = this[rowIdx][colIdx]
+    val neighbors = getNeighborHeights(rowIdx, colIdx)
+    return neighbors.any { it < tree }
+}
+
+private fun Forrest.getNeighborHeights(rowIdx: Int, colIdx: Int): List<Int> {
+    val row = this[rowIdx]
+    val topRow = getOrNull(rowIdx - 1).orEmpty()
+    val bottomRow = getOrNull(rowIdx - 1).orEmpty()
+
+    return listOf(
+        row.getOrNull(colIdx - 1) ?: 0, // left
+        topRow.getOrNull(colIdx) ?: 0, // top
+        row.getOrNull(colIdx + 1) ?: 0, // right
+        bottomRow.getOrNull(colIdx) ?: 0, // bottom
+    )
+}
+
+typealias Forrest = List<List<Int>>
