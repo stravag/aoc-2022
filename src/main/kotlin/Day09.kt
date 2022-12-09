4ec0f941cf74ee533258a77e78c5d2d74715d484
Ranil Wijeyratne <me@ranil.ch>
2022-12-09 08:56:41 +0100

    day 9 buggy first impl

--- src/main/kotlin/Day09.kt
+++ src/main/kotlin/Day09.kt
@@ -1,8 +1,10 @@
+import kotlin.math.abs
+
 fun main() {
     execute(
         day = "Day09",
         part1 = Part(
-            expectedTestResult = 0,
+            expectedTestResult = 13,
             expectedResult = 0,
             compute = ::compute1
         ),
@@ -15,9 +17,97 @@ fun main() {
 }
 
 private fun compute1(input: List<String>): Int {
-    return 0
+
+    fun Pair<Position, Position>.next(step: Step): Pair<Position, Position> {
+        val (tailStart, headStart) = this
+        var tailPos = tailStart
+        var headPos = headStart
+
+        List(step.steps) { step.direction }
+            .forEach { direction ->
+                headPos = headPos.move(direction)
+                tailPos = when (direction) {
+                    'R' -> {
+                        if (headPos.y == tailPos.y) {
+                            tailPos.move(direction)
+                        } else if (abs(headPos.x - tailPos.x) > 1) {
+                            Position(headPos.x - 1, headPos.y)
+                        } else {
+                            tailPos
+                        }
+                    }
+
+                    'L' -> {
+                        if (headPos.y == tailPos.y) {
+                            tailPos.move(direction)
+                        } else if (abs(headPos.x - tailPos.x) > 1) {
+                            Position(headPos.x + 1, headPos.y)
+                        } else {
+                            tailPos
+                        }
+                    }
+
+                    'U' -> {
+                        if (headPos.x == tailPos.x) {
+                            tailPos.move(direction)
+                        } else if (abs(headPos.y - tailPos.y) > 1) {
+                            Position(headPos.x, headPos.y - 1)
+                        } else {
+                            tailPos
+                        }
+                    }
+
+                    'D' -> {
+                        if (headPos.y == tailPos.y) {
+                            tailPos.move(direction)
+                        } else if (abs(headPos.y - tailPos.y) > 1) {
+                            Position(headPos.x, headPos.y + 1)
+                        } else {
+                            tailPos
+                        }
+                    }
+
+                    else -> throw IllegalArgumentException("unknown move $direction")
+                }
+            }
+
+        return tailPos to headPos
+    }
+
+    val start = Position(0, 0)
+    val visited = mutableSetOf(start)
+    var last = start to start
+    input
+        .map { Step.of(it) }
+        .forEach { step ->
+            last = last.next(step)
+            visited.add(last.first)
+        }
+
+    return visited.size
 }
 
 private fun compute2(input: List<String>): Int {
     return 0
+}
+
+private data class Position(val x: Int, val y: Int) {
+    fun move(direction: Char): Position {
+        return when (direction) {
+            'R' -> Position(x + 1, y)
+            'L' -> Position(x - 1, y)
+            'U' -> Position(x, y + 1)
+            'D' -> Position(x, y - 1)
+            else -> throw IllegalArgumentException("unknown move $direction")
+        }
+    }
+}
+
+data class Step(
+    val direction: Char,
+    val steps: Int,
+) {
+    companion object {
+        fun of(s: String): Step = s.split(" ").let { (m, i) -> Step(m.first(), i.toInt()) }
+    }
 }
