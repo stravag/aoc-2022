4dea1c58d9b08471eb4803ace3fa60e1858e271a
Ranil Wijeyratne <me@ranil.ch>
2022-12-09 08:27:48 +0100

    day 9 outline

---
+++ src/main/kotlin/Day09.kt
@@ -0,0 +1,23 @@
+fun main() {
+    execute(
+        day = "Day09",
+        part1 = Part(
+            expectedTestResult = 0,
+            expectedResult = 0,
+            compute = ::compute1
+        ),
+        part2 = Part(
+            expectedTestResult = 0,
+            expectedResult = 0,
+            compute = ::compute2
+        )
+    )
+}
+
+private fun compute1(input: List<String>): Int {
+    return 0
+}
+
+private fun compute2(input: List<String>): Int {
+    return 0
+}
