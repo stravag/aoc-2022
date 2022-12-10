e35017e1ab112d097fba5a381b19fe77cf77e1e9
Ranil Wijeyratne <me@ranil.ch>
2022-12-10 09:10:34 +0100

    day10.2 print correctly

--- src/test/kotlin/Day10.kt
+++ src/test/kotlin/Day10.kt
@@ -4,25 +4,22 @@ import kotlin.test.assertEquals
 object Day10 : AbstractDay() {
 
     @Test
-    fun tests() {
-        assertEquals(13140, compute1(testInput))
-        assertEquals(16880, compute1(puzzleInput))
-
-        assertEquals(146, compute2(testInput))
-        assertEquals(136, compute2(puzzleInput))
+    fun part1() {
+        assertEquals(13140, compute(testInput))
     }
 
-    private fun compute1(input: List<String>): Int {
+    @Test
+    fun part2() {
+        compute(puzzleInput)
+    }
+
+    private fun compute(input: List<String>): Int {
         val cpu = CPU()
         input.forEach {
             cpu.exec(it)
         }
 
         return cpu.currentSignal()
-    }
-
-    private fun compute2(input: List<String>): Int {
-        return input.size
     }
 
     class CPU(
@@ -52,9 +49,31 @@ object Day10 : AbstractDay() {
 
         private fun tick() {
             cycleCnt++
+            this.print()
             if (CHECK.contains(cycleCnt)) {
                 signal += cycleCnt * register
             }
+        }
+
+        private fun print() {
+            val sprite = Sprite(register)
+            val pixel = sprite.pixel(cycleCnt - 1)
+            print(pixel)
+            if (cycleCnt % 40 == 0) {
+                println()
+            }
+        }
+
+        private data class Sprite(val sprite: List<Char>) {
+            constructor(register: Int) : this(
+                List(40) {
+                    if (it in IntRange(register - 1, register + 1)) '#' else ' '
+                }
+            )
+
+            fun pixel(cycleCnt: Int): Char = sprite[cycleCnt % 40]
+
+            override fun toString(): String = sprite.joinToString(separator = "") { it.toString() }
         }
 
         companion object {
