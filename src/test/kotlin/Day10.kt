9a7d1c4bae1e331372dd332a0659b2194b8cb4b1
Ranil Wijeyratne <me@ranil.ch>
2022-12-10 08:31:54 +0100

    day10.1 done

--- src/test/kotlin/Day10.kt
+++ src/test/kotlin/Day10.kt
@@ -2,21 +2,63 @@ import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
 
 object Day10 : AbstractDay() {
-
+    
     @Test
     fun tests() {
-        assertEquals(146, compute1(testInput))
-        assertEquals(136, compute1(puzzleInput))
+        assertEquals(13140, compute1(testInput))
+        assertEquals(16880, compute1(puzzleInput))
 
         assertEquals(146, compute2(testInput))
         assertEquals(136, compute2(puzzleInput))
     }
 
     private fun compute1(input: List<String>): Int {
-        return input.size
+        val cpu = CPU()
+        input.forEach {
+            cpu.exec(it)
+        }
+
+        return cpu.currentSignal()
     }
 
     private fun compute2(input: List<String>): Int {
         return input.size
+    }
+
+    class CPU(
+        private var register: Int = 1,
+        private var cycleCnt: Int = 0,
+        private var signal: Int = 0,
+    ) {
+        fun exec(cmd: String) {
+            val parts = cmd.split(" ")
+            when (parts.first()) {
+                "noop" -> noop()
+                "addx" -> add(parts[1].toInt())
+            }
+        }
+
+        fun currentSignal(): Int = signal
+
+        private fun noop() {
+            tick()
+        }
+
+        private fun add(i: Int) {
+            tick()
+            tick()
+            register += i
+        }
+
+        private fun tick() {
+            cycleCnt++
+            if (CHECK.contains(cycleCnt)) {
+                signal += cycleCnt * register
+            }
+        }
+
+        companion object {
+            private val CHECK = listOf(20, 60, 100, 140, 180, 220)
+        }
     }
 }
