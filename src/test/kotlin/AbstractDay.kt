d1ea549b82f57d3db914e1e91a2dc3ff63e496a9
Ranil Wijeyratne <me@ranil.ch>
2022-12-10 20:26:58 +0100

    move test files to subfolders for easier copy/pase

--- src/test/kotlin/AbstractDay.kt
+++ src/test/kotlin/AbstractDay.kt
@@ -1,9 +1,8 @@
 import java.io.File
-import kotlin.math.abs
-import kotlin.test.assertEquals
 
 abstract class AbstractDay {
 
-    val puzzleInput get() = File("src/test/resources/${this::class.java.simpleName}.txt").readLines()
-    val testInput get() = File("src/test/resources/${this::class.java.simpleName}_test.txt").readLines()
+    private val day: String get() = this::class.java.simpleName
+    val puzzleInput: List<String> get() = File("src/test/resources/$day/puzzle.txt").readLines()
+    val testInput get() = File("src/test/resources/$day/test.txt").readLines()
 }
