82f00eb984573e18ee897e11ca4ddb801ddbdbf7
Ranil Wijeyratne <me@ranil.ch>
2022-12-11 08:13:40 +0100

    day11 parse monkeys

--- src/test/kotlin/Day11.kt
+++ src/test/kotlin/Day11.kt
@@ -17,10 +17,55 @@ object Day11 : AbstractDay() {
         return input.size
     }
 
-    class Monkey {
+    class Monkey(
+        val items: List<Int>,
+        val operation: (Int) -> Int,
+        val divisor: Int,
+        val trueMonkey: Int,
+        val falseMonkey: Int
+    ) {
+
         companion object {
             fun of(lines: List<String>): Monkey {
-                return Monkey()
+                val items = extractItems(lines[1])
+                val operation = extractOperation(lines[2])
+                val divisor = extractDivisor(lines[3])
+                val (trueMonkey, falseMonkey) = extractTargetMonkeys(lines[4], lines[5])
+                return Monkey(items, operation, divisor, trueMonkey, falseMonkey)
+            }
+
+            private fun extractItems(s: String): List<Int> {
+                return s
+                    .split(": ")[1]
+                    .split(",")
+                    .map { it.trim().toInt() }
+            }
+
+            private fun extractOperation(s: String): (Int) -> Int {
+                val opStr = s.split(" = ")[1].split(" ")
+                val a = opStr[0].toIntOrNull()
+                val b = opStr[2].toIntOrNull()
+                val operation: (Int, Int) -> Int = when (opStr[1]) {
+                    "*" -> Int::times
+                    "+" -> Int::plus
+                    else -> throw IllegalArgumentException()
+                }
+                return {
+                    val opA = a ?: it
+                    val opB = b ?: it
+                    operation(opA, opB)
+                }
+            }
+
+            private fun extractDivisor(s: String): Int {
+                return s.split(" ").last().toInt()
+            }
+
+            private fun extractTargetMonkeys(trueMonkey: String, falseMonkey: String): Pair<Int, Int> {
+                return Pair(
+                    trueMonkey.split(" ").last().toInt(),
+                    falseMonkey.split(" ").last().toInt(),
+                )
             }
         }
     }
