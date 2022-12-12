d1b1f7fb0a7fea168b9e269e79850200c6e5a051
Ranil Wijeyratne <ranil.wijeyratne@ergon.ch>
2022-12-12 08:25:20 +0100

    day12 scaffold

---
+++ src/test/kotlin/Day12.kt
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+object Day12 : AbstractDay() {
+
+    @Test
+    fun part1() {
+        assertEquals(0, compute(testInput))
+        assertEquals(0, compute(puzzleInput))
+    }
+
+    @Test
+    fun part2() {
+        assertEquals(0, compute(testInput))
+        assertEquals(0, compute(puzzleInput))
+    }
+
+    private fun compute(input: List<String>): Int {
+        return 0
+    }
+}
