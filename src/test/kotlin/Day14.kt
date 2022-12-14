d5800134922734d8b83e6e369a0175dad1f0ee1c
Ranil Wijeyratne <ranil.wijeyratne@ergon.ch>
2022-12-14 16:26:37 +0100

    day14 wip

--- src/test/kotlin/Day14.kt
+++ src/test/kotlin/Day14.kt
@@ -17,10 +17,43 @@ object Day14 : AbstractDay() {
 
 
     private fun compute1(input: List<String>): Int {
+        val rocks = input.flatMap { line ->
+            line.split(" -> ").windowed(size = 2) {
+                val (start, end) = V.of(it)
+                val rocks = getRocks(start, end)
+                rocks
+            }.flatten()
+        }
+
+        Mountain(rocks.toSet())
+        val sandStart = V(500, 0)
         return input.size
     }
 
     private fun compute2(input: List<String>): Int {
         return input.size
+    }
+
+    private data class Mountain(val rocks: Set<V>) {
+
+    }
+
+    fun getRocks(start: V, end: V): List<V> {
+        // fix range
+        val xRocks = if (start.x != end.x) (start.x..end.x).map { V(it, start.y) } else emptyList()
+        val yRocks = if (start.y != end.y) (start.y..end.y).map { V(start.x, it) } else emptyList()
+        return xRocks + yRocks
+    }
+
+    data class V(val x: Int, val y: Int) {
+        companion object {
+
+
+            fun of(strings: List<String>): List<V> = strings.map { of(it) }
+            private fun of(string: String): V {
+                val (x, y) = string.split(",").map { it.toInt() }
+                return V(x, y)
+            }
+        }
     }
 }
