21fcd65fb9d9832079bdf72bd39c3fba7be5f4b0
Ranil Wijeyratne <me@ranil.ch>
2022-12-22 00:56:31 +0100

    day22 scaffold

---
+++ src/test/kotlin/Day22.kt
@@ -0,0 +1,34 @@
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class Day22 : AbstractDay() {
+
+    @Test
+    fun part1Test() {
+        assertEquals(0, compute1(testInput))
+    }
+
+    @Test
+    fun part1Puzzle() {
+        assertEquals(0, compute1(puzzleInput))
+    }
+
+    @Test
+    fun part2Test() {
+        assertEquals(0, compute2(testInput))
+    }
+
+    @Test
+    fun part2Puzzle() {
+        assertEquals(0, compute2(puzzleInput))
+    }
+
+    private fun compute1(input: List<String>): Long {
+        TODO()
+    }
+
+
+    private fun compute2(input: List<String>): Long {
+        TODO()
+    }
+}
