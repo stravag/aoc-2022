e66c595cbf4d3fa156817d86b9587fff011c11ad
Ranil Wijeyratne <ranil.wijeyratne@ergon.ch>
2022-12-21 14:04:54 +0100

    day21.2 wip

--- src/test/kotlin/Day21.kt
+++ src/test/kotlin/Day21.kt
@@ -24,7 +24,7 @@ class Day21 : AbstractDay() {
     }
 
     private fun compute1(input: List<String>): Long {
-        val monkeys = parse(input)
+        val monkeys = parse1(input)
         val rootMonkey = monkeys.getValue("root")
 
         return getAnswer(rootMonkey, monkeys)
@@ -42,10 +42,30 @@ class Day21 : AbstractDay() {
     }
 
     private fun compute2(input: List<String>): Long {
-        TODO()
+        val monkeys = parse2(input)
+
+        val rootMonkey = monkeys.getValue("root") as OperationMonkey
+
+        val b = getAnswer(monkeys.getValue(rootMonkey.b), monkeys)
+
+        return requiredNumberForEquality(b, monkeys.getValue(rootMonkey.a), monkeys)
     }
 
-    private fun parse(input: List<String>): Map<String, Monkey> {
+    private fun requiredNumberForEquality(number: Long, monkey: Monkey, monkeys: Map<String, Monkey>): Long {
+        return when (monkey) {
+            is NumberMonkey -> monkey.number
+            is OperationMonkey -> {
+                val a = monkeys[monkey.a] ?: return number
+                val b = monkeys[monkey.b] ?: return number
+
+                val answerA = getAnswer(a, monkeys)
+                val answerB = getAnswer(b, monkeys)
+                val answer = monkey.operation(answerA, answerB)
+            }
+        }
+    }
+
+    private fun parse1(input: List<String>): Map<String, Monkey> {
         val monkeys = input.map {
             val (name, operationString) = it.split(": ")
             if (operationString.toLongOrNull() == null) {
@@ -66,6 +86,31 @@ class Day21 : AbstractDay() {
         return monkeys.associateBy { it.name }
     }
 
+    private fun parse2(input: List<String>): Map<String, Monkey> {
+        val monkeys = input.mapNotNull {
+            val (name, operationString) = it.split(": ")
+            if (operationString.toLongOrNull() == null) {
+                val (a, operator, b) = operationString.split(" ")
+                val operation: (Long, Long) -> Long = when (operator) {
+                    "+" -> Long::plus
+                    "-" -> Long::minus
+                    "*" -> Long::times
+                    "/" -> Long::div
+                    else -> throw IllegalArgumentException("unexpected operator $operator")
+                }
+                OperationMonkey(name, a, b, operation)
+            } else {
+                if (name == "humn") {
+                    null // we skip this on purpose
+                } else {
+                    NumberMonkey(name, operationString.toLong())
+                }
+            }
+        }
+
+        return monkeys.associateBy { it.name }
+    }
+
     sealed interface Monkey {
         val name: String
     }
@@ -79,6 +124,6 @@ class Day21 : AbstractDay() {
         override val name: String,
         val a: String,
         val b: String,
-        val operation: (Long, Long) -> Long
+        val operation: (Long, Long) -> Long,
     ) : Monkey
 }
